72e25e5cba0c47b177ce33252e0f90360d41d267
youlheek <youlheek@gmail.com>
2025-02-24 15:52:12 +0900

    Initial commit

---
+++ src/main/java/com/rebootcrew/trendly/TrendlyApplication.java
@@ -0,0 +1,13 @@
+package com.rebootcrew.trendly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebootcrew/trendly/TrendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebootcrew.trendly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

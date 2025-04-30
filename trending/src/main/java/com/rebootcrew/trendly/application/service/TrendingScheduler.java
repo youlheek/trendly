c0d0981b4cc1cb9369c23729da1d3b6d684c6ccc
Jinhan Han Jeremy <hanjinhan89@gmail.com>
2025-04-30 15:53:36 +0900

    jinhan Han's work
       feat      : 검색어 리스트 스케쥴에 맞춰 db에 insert
       fix       : 검색량에 맞춰 순위 업데이트
    없음)

--- trending/src/main/java/com/rebootcrew/trendly/application/service/TrendingScheduler.java
+++ trending/src/main/java/com/rebootcrew/trendly/application/service/TrendingScheduler.java
@@ -15,7 +15,7 @@ public class TrendingScheduler {
 
     // 매일 오전 12시 5분 실행
     //TotalList 업데이트
-    @Scheduled(cron = "0 51 15 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 59 10 * * *", zone = "Asia/Seoul")
     // 서버가 켜진 뒤부터 7시 10분마다 계속 반복하고 싶다면:
 //    @Scheduled(fixedRate = 430_000)  // 7분 10초 = 430,000ms
 //    @PostConstruct  //어플리케이션 실행시 구동
